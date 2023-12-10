9872ba9d23fefc1449ff995a6aeb52ebff3201c1
RafaelSimasS <rafaeldesousasimas@gmail.com>
2023-12-10 00:24:35 -0300

    fix: vercel deployment port

--- dist/server.js
+++ dist/server.js
@@ -7,12 +7,14 @@ const express_1 = __importDefault(require("express"));
 const UserAuth_1 = __importDefault(require("./routes/UserAuth"));
 const PreferencesHandlers_1 = __importDefault(require("./routes/PreferencesHandlers"));
 const app = (0, express_1.default)();
-const port = 5000;
+const port = 8080;
 app.use(express_1.default.json());
 app.use(UserAuth_1.default);
 app.use(PreferencesHandlers_1.default);
 app.get("/", (_, res) => {
     res.status(200).send('<div align="center"><h1>Visitour API v1.0</h1></div>');
 });
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.listen(port, () => {
+    return console.log(`Running on port ${port}`);
+});
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import UserAuthRoutes from "./routes/UserAuth";
 import PreferencesRouter from "./routes/PreferencesHandlers";
 
 const app = express();
-const port = 5000;
+const port = 8080;
 
 app.use(express.json());
 app.use(UserAuthRoutes);
@@ -13,4 +13,6 @@ app.use(PreferencesRouter);
 app.get("/", (_, res) => {
   res.status(200).send('<div align="center"><h1>Visitour API v1.0</h1></div>');
 });
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.listen(port, () => {
+  return console.log(`Running on port ${port}`);
+});
